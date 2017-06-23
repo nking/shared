06a5ecff58d92e70d929a7d0dcd368a1e4675e18
nichole <nichole@climbwithyourfeet.com>
2017-06-22 22:17:03 -0700

    adding a method to convert pixel sets to pixel index sets

--- src/main/java/algorithms/util/PixelHelper.java
+++ src/main/java/algorithms/util/PixelHelper.java
@@ -1,5 +1,9 @@
 package algorithms.util;
 
+import gnu.trove.set.TIntSet;
+import gnu.trove.set.hash.TIntHashSet;
+import java.util.Set;
+
 /**
  *
  * @author nichole
@@ -18,5 +22,13 @@ public class PixelHelper {
         int[] outputXY) {
         outputXY[1] = pixIdx/width;
         outputXY[0] = pixIdx - (outputXY[1] * width);
+    }
+
+    public TIntSet convert(Set<PairInt> points, int width) {
+        TIntSet set = new TIntHashSet();
+        for (PairInt p : points) {
+            set.add(toPixelIndex(p, width));
+        }
+        return set;
     }
 }
