7bb9a763594c4bb420db1a75d61a53cf50ab7b46
nichole <nichole@climbwithyourfeet.com>
2017-06-23 23:32:35 -0700

    added a 1D atrous wavelet transform

---
+++ src/main/java/algorithms/signalProcessing/ATrousWaveletTransform1D.java
@@ -0,0 +1,280 @@
+package algorithms.signalProcessing;
+
+import algorithms.util.OneDFloatArray;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author nichole
+ */
+public class ATrousWaveletTransform1D {
+   
+    /**
+     * The a trous algorithm is a fast implementation of a wavelet transform 
+     * with no downsampling.   It is non-orthogonal, semi-linear runtime
+     * complexity, is invariant under translation, and the transform is 
+     * isotropic.
+     * Implemented from pseudocode in http://www.multiresolution.com/svbook.pdf
+       The scaling function used is the lower resolution choice, the triangle
+       * function.
+       * <pre>
+       * The method uses recursive convolution operations, including previous
+       * result to make next.
+       * </pre>
+     * @param input
+     * @param outputTransformed
+     * @param outputCoeff 
+     */
+    public void calculateWithTriangleScalingFunction(float[] input,
+        List<OneDFloatArray> outputTransformed, List<OneDFloatArray> outputCoeff) {
+        
+        int imgDimen = input.length;
+
+        int nr = (int)(Math.log(imgDimen)/Math.log(2));
+
+        TriangleFunction1D scalingFunction = new TriangleFunction1D();
+        
+        OneDFloatArray cp = new OneDFloatArray(Arrays.copyOf(input, input.length));
+        outputTransformed.add(cp);
+        
+        OneDFloatArray cf = new OneDFloatArray(new float[input.length]);
+        outputCoeff.add(cf);
+
+        for (int j = 0; j < nr; ++j) {
+            
+            OneDFloatArray cJ = outputTransformed.get(j);
+ 
+            OneDFloatArray cJPlus1 = new OneDFloatArray(
+                scalingFunction.calculateNextLevel(cJ.a, j));
+           
+            outputTransformed.add(cJPlus1);
+            
+            // c_(j,k) − c_(j+1,k)
+            float[] s = Arrays.copyOf(cJ.a, cJ.a.length);
+            for (int jj = 0; jj < s.length; ++jj) {
+                s[jj] -= cJPlus1.a[jj];
+            }
+            OneDFloatArray wJPlus1 = new OneDFloatArray(s);
+          
+            outputCoeff.add(wJPlus1);
+        }
+    }
+
+    /**
+     * The a trous algorithm is a fast implementation of a wavelet transform 
+     * with no downsampling.   It is non-orthogonal, has semi-linear runtime
+     * complexity, is invariant under translation, and the transform is 
+     * isotropic.
+     * Implemented from pseudocode in http://www.multiresolution.com/svbook.pdf
+     * The scaling function used is the higher resolution choice, the 3rd order 
+     * B Spline function.
+     * <pre>
+     * The method uses recursive convolution operations, including previous
+       * result to make next.
+       * Each convolution uses two passes of one dimensional binomial kernels,
+       * starting with the equivalent of sigma=1.
+       * For each step, the equivalent resulting sigma is from 
+       * sigma^2 = sigma_1^2 + sigma_2^2.
+       * 
+       * outputTransformed[1] = sigma = 1 convolution
+       * outputTransformed[2] = sqrt( (1)^2 + (1)^2 ) = sqrt(2) convolution
+       * outputTransformed[3] = sqrt( 2 + 1 ) = sqrt(3) convolution
+       * outputTransformed[4] = sqrt( 3 + 1 ) = sqrt(4) = 2 convolution
+       * outputTransformed[5] = sqrt( 4 + 1 ) = sqrt(5) convolution
+       * outputTransformed[6] = sqrt( 5 + 1 ) = sqrt(6) convolution
+       * ...
+       * outputTransformed[8] = sqrt( 8 + 1 ) = sqrt(9) = 3 convolution
+       * </pre>
+     * @param input
+     * @param outputTransformed
+     * @param outputCoeff 
+     */
+    public void calculateWithB3SplineScalingFunction(float[] input,
+        List<OneDFloatArray> outputTransformed, List<OneDFloatArray> outputCoeff) {
+
+        int imgDimen = input.length;
+
+        int nr = (int)(Math.log(imgDimen)/Math.log(2));
+
+        B3SplineFunction1D scalingFunction = new B3SplineFunction1D();
+        
+        OneDFloatArray cp = new OneDFloatArray(Arrays.copyOf(input, input.length));
+        outputTransformed.add(cp);
+        
+        OneDFloatArray cf = new OneDFloatArray(new float[input.length]);
+        outputCoeff.add(cf);
+
+        for (int j = 0; j < nr; ++j) {
+            
+            OneDFloatArray cJ = outputTransformed.get(j);
+ 
+            OneDFloatArray cJPlus1 = new OneDFloatArray(
+                scalingFunction.calculate(cJ.a));
+           
+            outputTransformed.add(cJPlus1);
+            
+            // c_(j,k) − c_(j+1,k)
+            float[] s = Arrays.copyOf(cJ.a, cJ.a.length);
+            for (int jj = 0; jj < s.length; ++jj) {
+                s[jj] -= cJPlus1.a[jj];
+            }
+            OneDFloatArray wJPlus1 = new OneDFloatArray(s);
+            
+            outputCoeff.add(wJPlus1);
+        }
+        
+    }
+    
+    /**
+     * The a trous algorithm is a fast implementation of a wavelet transform 
+     * with no downsampling.   It is non-orthogonal, has semi-linear runtime
+     * complexity, is invariant under translation, and the transform is 
+     * isotropic.
+     * Implemented from pseudocode in http://www.multiresolution.com/svbook.pdf
+     * The scaling function used is the higher resolution choice, the 3rd order 
+     * B Spline function.
+     * <pre>
+     * The method uses recursive convolution operations, including previous
+       * result to make next.
+       * Each convolution uses two passes of one dimensional binomial kernels,
+       * starting with the equivalent of sigma=1.
+       * For each step, the equivalent resulting sigma is from 
+       * sigma^2 = sigma_1^2 + sigma_2^2.
+       * 
+       * This method takes an argument stopIter to indicate that only stopIter
+       * number of iterations are needed.  For instance, to retrieve only the
+       * first populated wavelet, use stopIter = 2 (the first is initialization,
+       * and the second is the calculation).
+       * </pre>
+     * @param input
+     * @param outputTransformed
+     * @param outputCoeff
+     * @param stopIter
+     */
+    public void calculateWithB3SplineScalingFunction(float[] input,
+        List<OneDFloatArray> outputTransformed, List<OneDFloatArray> outputCoeff,
+        int stopIter) {
+
+        int imgDimen = input.length;
+
+        int nr = (int)(Math.log(imgDimen)/Math.log(2));
+
+        if (nr > stopIter) {
+            nr = stopIter;
+        }
+        
+        B3SplineFunction1D scalingFunction = new B3SplineFunction1D();
+        
+        OneDFloatArray cp = new OneDFloatArray(Arrays.copyOf(input, input.length));
+        outputTransformed.add(cp);
+        
+        OneDFloatArray cf = new OneDFloatArray(new float[input.length]);
+        outputCoeff.add(cf);
+
+        for (int j = 0; j < nr; ++j) {
+            
+            OneDFloatArray cJ = outputTransformed.get(j);
+ 
+            OneDFloatArray cJPlus1 = new OneDFloatArray(
+                scalingFunction.calculate(cJ.a));
+           
+            outputTransformed.add(cJPlus1);
+            
+            // c_(j,k) − c_(j+1,k)
+            float[] s = Arrays.copyOf(cJ.a, cJ.a.length);
+            for (int jj = 0; jj < s.length; ++jj) {
+                s[jj] -= cJPlus1.a[jj];
+            }
+            OneDFloatArray wJPlus1 = new OneDFloatArray(s);
+            
+            outputCoeff.add(wJPlus1);
+        }
+        
+    }
+   
+    public float[] reconstruct(float[] c0, List<OneDFloatArray> mmCoeff) {
+
+        int nr = mmCoeff.size();
+
+        OneDFloatArray output = new OneDFloatArray(Arrays.copyOf(c0, c0.length));
+
+        for (int j = 0; j < nr; ++j) {
+           
+            float[] add = mmCoeff.get(j).a;
+            for (int jj = 0; jj < add.length; ++jj) {
+                output.a[jj] += add[jj];
+            }
+        }
+
+        return output.a;
+    }
+    
+    /**
+     * Following
+     * "Edge-Optimized À-Trous Wavelets for Local Contrast Enhancement with 
+     * Robust Denoising" by Johannes Hanika, Holger Dammertz, and Hendrik Lensch
+     * https://jo.dreggn.org/home/2011_atrous.pdf
+     * to estimate del c_i_jj as part of creating an error image.
+     * The authors calculate gradient c_i_jj using Cranley Patterson rotation 
+       sampling within the A Trous B3Spline window (which is 25 pixels).
+       
+       This looks a little like calculating auto-correlation, except not wanting 
+       the center pixel as the fixed first pixel of the difference.
+       
+       If gradient c_i_jj is meant to be a measure of local image noise, would 
+       presumably want to select only differences between adjacent pixel pairs.
+       So the use of Cranley Patterson rotation must be in selecting the second
+       point using an offset chosen from the vector U of values.
+       That offset is applied uniformly to the set to help choose the 2nd point.
+       The universe of offsets U can only be the offsets to result in the 8
+       neighbor region.
+        
+       Not sure, but I think that is what the authors implemented.
+        
+       Given to this method are the center pixel index for the A Trous window
+       and the offsets as dx and dy chosen from the universe U of 8 neigbhor
+       offsets.
+        
+       For each pixel in the window, will determine its intensity difference 
+       from the pixel and the pixel that is it's coordinates plus the offsets.
+       The result returned will be the average of those.
+       
+       Note that another paper 
+       ("Efficient Multidimensional Sampling" by Kollig and Keller, 
+       http://www.uni-kl.de/AG-Heinrich/EMS.pdf)
+       suggests different sampling methods, so may change this in the future.
+        
+     * @return 
+     */
+    private float estimateLocalNoise(float[] data, int pixIdx, int xOffset) {
+        
+        int len = data.length;
+        
+        float v = data[pixIdx];
+        
+        int count = 0;
+        float diff = 0;
+        // iterate within window to find first pixel
+        for (int dx = -2; dx <= 2; ++dx) {
+            int x1 = pixIdx + dx;
+            if (x1 < 0 || x1 > (len - 1)) {
+                continue;
+            }
+                          
+            int x2 = x1 + xOffset;
+            if ((x2 < 0) || (x2 > (len - 1))) {
+                continue;
+            }
+
+            diff += Math.abs(data[x1] - data[x2]);
+
+            count++;
+        }
+        assert(count > 0);
+        
+        diff /= (float)count;
+        
+        return diff;
+    }
+}
---
+++ src/test/java/algorithms/signalProcessing/ATrousWaveletTransform1DTest.java
@@ -0,0 +1,235 @@
+package algorithms.signalProcessing;
+
+import algorithms.misc.MiscMath0;
+import algorithms.util.OneDFloatArray;
+import algorithms.util.PolygonAndPointPlotter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author nichole
+ */
+public class ATrousWaveletTransform1DTest extends TestCase {
+    
+    public ATrousWaveletTransform1DTest(String testName) {
+        super(testName);
+    }
+
+    public void testTriangleFunction() {
+        
+        ATrousWaveletTransform1D wave = new ATrousWaveletTransform1D();
+        float fwhm;
+        
+        float sigma = 1.f;
+        
+        float[] a = createGaussian(sigma, 0);
+        
+        //FWHM is approx 2.35 * sigma
+        fwhm = measureFWHM(a);
+        System.out.println("fwhm=" + fwhm);
+        assertTrue(Math.abs(fwhm - 2.35*sigma) <= 1);
+        
+        List<OneDFloatArray> outputTransformed = new 
+            ArrayList<OneDFloatArray>();
+        List<OneDFloatArray> outputCoeff = new 
+            ArrayList<OneDFloatArray>();
+        wave.calculateWithTriangleScalingFunction(a, outputTransformed, 
+            outputCoeff);
+      
+        for (OneDFloatArray b : outputTransformed) {
+            //System.out.println("b=" + Arrays.toString(b.a));
+            fwhm = measureFWHM(b.a);
+            System.out.println("fwhm=" + fwhm);
+            
+            float sigma2 = fwhm/2.35f;
+            double s = Math.sqrt(sigma2*sigma2 - sigma*sigma);
+            if (!Double.isNaN(s)) {
+                System.out.println("implied sigma for level convolution=" + 
+                    s + " resulting in " + sigma2);
+            }
+        }
+        
+        /*
+        s^2 = sigma^2 + 0.707^2 --> s2-s02
+        s = sqrt(sigma^2 + 0.707^2)
+        */
+    //    System.out.println("a=" + Arrays.toString(a));
+    //    System.out.println("last=" + Arrays.toString(
+    //        outputTransformed.get(outputTransformed.size() - 1).a));
+    
+    }
+
+    public void testB3SplineScalingFunction() throws IOException {
+    
+        ATrousWaveletTransform1D wave = new ATrousWaveletTransform1D();
+        float fwhm;
+        
+        float sigma = 1.f;
+        
+        float[] a = createGaussian(sigma, 0);
+        
+        /*
+        using a middle line bias of 5,
+           will subtract the gaussian from 5,
+           then next segment added to 5,
+           then next segment subtracted from 5
+        */
+        int n = a.length;
+        float bias = 5.f;
+        
+        float[] input = new float[n*3];
+        for (int i = 0; i < n; ++i) {
+            input[i] = bias + 4.f*a[i];
+        }
+        int idx = n;
+        for (int i = 0; i < n; ++i) {
+            input[idx] = bias - 4.f*a[i];
+            idx++;
+        }
+        for (int i = 0; i < n; ++i) {
+            input[idx] = bias + 4.f*a[i];
+            idx++;
+        }
+        
+        List<OneDFloatArray> outputTransformed = new 
+            ArrayList<OneDFloatArray>();
+        List<OneDFloatArray> outputCoeff = new 
+            ArrayList<OneDFloatArray>();
+        wave.calculateWithB3SplineScalingFunction(input, outputTransformed, 
+            outputCoeff);
+      
+        /*
+        System.out.println("input=" + Arrays.toString(input));
+        System.out.println("last=" + Arrays.toString(
+            outputTransformed.get(outputTransformed.size() - 1).a));
+          
+        float[] x = new float[input.length];
+        for (int i = 0; i < x.length; ++i) {
+            x[i] = i;
+        }
+        
+        PolygonAndPointPlotter plotter = new PolygonAndPointPlotter();
+        plotter.addPlot(0.f, input.length, 0.f, 2.f*bias, 
+            x, input, x, input, 
+            "input");
+        plotter.addPlot(0.f, input.length, 0.f, 2.f*bias, 
+            x, 
+            outputTransformed.get(outputTransformed.size() - 1).a, 
+            x, 
+            outputTransformed.get(outputTransformed.size() - 1).a, 
+            "transformed");
+        System.out.println(plotter.writeFile());
+        */
+        
+        int min0 = MiscMath0.findYMaxIndex(input);
+        int min1 = MiscMath0.findYMaxIndex(
+            outputTransformed.get(outputTransformed.size() - 1).a);
+        assertEquals(min0, min1);
+    }
+
+    public void testReconstruct() {
+        ATrousWaveletTransform1D wave = new ATrousWaveletTransform1D();
+        
+    }
+    
+    /*
+     The code below is from project
+     http://github.com/nking/curvature-scale-space-corners-and-transformations
+     w/ Copyright (c) 2014 Climb With Your Feet
+     and using The MIT License (MIT)
+     */
+    
+    private float[] createGaussian(float sigma, float mu) {
+        
+        float normalization = (float)(sigma * Math.sqrt(2.f * Math.PI));
+        
+        float hwi = estimateHWZI(sigma, 0.001f);
+        if (hwi < 0) {
+            hwi *= -1.f;
+        }
+        int halfWidthInPixels = (int)Math.ceil(hwi);
+        
+        int start = -1*halfWidthInPixels;
+        int stopExcl = halfWidthInPixels + 1;
+        
+        float d, dsq;
+       
+        int nPoints = stopExcl - start;
+        
+        float[] yPoints = new float[nPoints];
+        int count = 0;
+        for (int i = start; i < stopExcl; i++) {
+            
+            float x = i;
+            d = (x - mu);
+            dsq = d*d;
+            
+            float y = (float) Math.exp(-1.f * dsq/(2.f * sigma * sigma));
+            
+            yPoints[count] = y/normalization;
+                        
+            count++;
+        }
+                                
+        return yPoints;
+    }
+    
+    private float estimateHWZI(float sigma, float fractionMax) {
+        
+        /*
+           exp( (-(x0 - mu)^2)/2o~^2) / exp( (-(xcenter - mu)^2)/2o~^2) = fractionMax
+        
+           where xcenter = mu is the center of the gaussian
+        
+           exp( (-(x0 - mu)^2)/2o~^2) / 1 = fractionMax
+        
+           (-(x0 - mu)^2)/2o~^2) = ln(fractionMax)
+        
+           -(x0 - mu)^2 = -1*(2o~^2 * ln(fractionMax))
+           
+           x0 - mu = math.sqrt(-1*(2o~^2 * ln(fractionMax)))
+        
+           x0 = mu + o~ * math.sqrt(-1*(2 * ln(fractionMax)))
+        
+        */
+
+        float x0 = (float)(sigma * Math.sqrt(-2. * Math.log(fractionMax)));
+                
+        return x0;
+    }
+    
+    private float measureFWHM(float[] x) {
+                
+        int xMaxIdx = MiscMath0.findYMaxIndex(x);
+        float xMax = x[xMaxIdx];
+                
+        int xHalfMax0Idx = -1;
+        int xHalfMax1Idx = -1;
+        for (int i = xMaxIdx; i > -1; i--) {
+            if (x[i] > (xMax / 2.)) {
+                xHalfMax0Idx = i;
+            } else {
+                break;
+            }
+        }
+        for (int i = xMaxIdx - 1; i < x.length; i++) {
+            if (x[i] > (xMax / 2.)) {
+                xHalfMax1Idx = i;
+            } else {
+                break;
+            }
+        }
+        
+        System.out.println("max=" + xMax + "\n" +
+            " " + xHalfMax0Idx + " : " + xMaxIdx + " : " + xHalfMax1Idx);
+        
+        if ((xHalfMax1Idx == -1) || (xHalfMax0Idx == -1)) {
+            return -1;
+        }
+        return xHalfMax1Idx - xHalfMax0Idx;
+    }
+}
