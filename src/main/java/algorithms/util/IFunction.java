ba6a2fce850364641fa85be2c24faa796421b41f
nichole <nichole@climbwithyourfeet.com>
2020-05-27 19:34:40 -0700

    added comments to IFunction.java

--- src/main/java/algorithms/util/IFunction.java
+++ src/main/java/algorithms/util/IFunction.java
@@ -8,7 +8,9 @@ package algorithms.util;
  */
 public interface IFunction {
     
-    /** evaluates the objective at this set of coefficients.
+    /** evaluates the objective at this set of coefficients.  NOTE that any data
+     * specific to a function, such as input multi-dimensional observations,
+     * can be part of the concrete implementation  instance variables.
     */
     double f (double[] coeffs);
     
