1c47aa607e355202b3787d01e022c748f782bb04
nichole <nichole@climbwithyourfeet.com>
2020-09-18 13:23:27 -0700

    unfinished multivariate uniform distribution sampling method

--- src/main/java/algorithms/sampling/MultivariateUniformDistribution.java
+++ src/main/java/algorithms/sampling/MultivariateUniformDistribution.java
@@ -1,6 +1,7 @@
 package algorithms.sampling;
 
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 /**
  * create random vectors uniformly distributed on the surface of the unit n-1 
@@ -23,50 +24,82 @@ public class MultivariateUniformDistribution {
     */
     
     /**
-     * random uniform points in an n-dimensional uniform unit standard ball.
-     * @param nDimensions
-     * @return
+     * generate random uniform points on an n-dimensional unit standard hypersphere.
+     * NOTE: to generate multiple samples, invoke the overloaded method multiple times.
+     * <pre>
+     * paper containing references:
+     * "Efficiently sampling vectors and coordinates from the n-sphere and n-ball"
+    Aaron R. Voelker, Jan Gosmann, Terrence C. Stewart Centre for Theoretical Neuroscience – Technical Report
+    January 4, 2017
+     * https://www.researchgate.net/profile/Aaron_Voelker/publication/312056739_Efficiently_sampling_vectors_and_coordinates_from_the_n-sphere_and_n-ball/links/586d3e1208ae6eb871bce1d6/Efficiently-sampling-vectors-and-coordinates-from-the-n-sphere-and-n-ball.pdf
+     * </pre>
+     * @param d number of dimensions.
+     * @return vector of size d+1 
      * @throws NoSuchAlgorithmException 
      */
-    public static double[] generateUnitStandardSample(int nDimensions) throws NoSuchAlgorithmException {
+    public static double[] generateUnitStandardOnNSphere(int d) 
+        throws NoSuchAlgorithmException {
         
-        //also see https://www.sciencedirect.com/science/article/pii/S0047259X10001211
-        /*Ondecompositionalalgorithmsforuniformsamplingfromn-spheresandn-balls
-        Harman, and Lacko
-        Journal of Multivariate Analysis 101 (2010) 2297–2304
-        */
+         SecureRandom rand = SecureRandom.getInstanceStrong();
+        long seed = System.nanoTime();
+        //System.out.println("SEED=" + seed);
+        rand.setSeed(seed);
         
-        // generate nDimensions individ random standard normal N(0,1)        
-        double[] u = UnivariateNormalDistribution.randomSampleOfUnitStandard(nDimensions);
+        return generateUnitStandard(d, rand, true);
+    }
+    
+    /**
+     * generate random uniform points in an n-dimensional unit standard ball.
+     * NOTE: to generate multiple samples, invoke the overloaded method multiple times.
+     * <pre>
+     * paper containing references:
+     * "Efficiently sampling vectors and coordinates from the n-sphere and n-ball"
+    Aaron R. Voelker, Jan Gosmann, Terrence C. Stewart Centre for Theoretical Neuroscience – Technical Report
+    January 4, 2017
+     * https://www.researchgate.net/profile/Aaron_Voelker/publication/312056739_Efficiently_sampling_vectors_and_coordinates_from_the_n-sphere_and_n-ball/links/586d3e1208ae6eb871bce1d6/Efficiently-sampling-vectors-and-coordinates-from-the-n-sphere-and-n-ball.pdf
+     * </pre>
+     * @param d number of dimensions.
+     * @return vector of size d+1 
+     * @throws NoSuchAlgorithmException 
+     */
+    public static double[] generateUnitStandardInNBall(int d) 
+        throws NoSuchAlgorithmException {
         
-        // then normalization of each is the sqrt(sum of squares of all)
-        double sqSumSq = 0;
-        for (double v : u) {
-            sqSumSq += (v*v);
-        }
+         SecureRandom rand = SecureRandom.getInstanceStrong();
+        long seed = System.nanoTime();
+        //System.out.println("SEED=" + seed);
+        rand.setSeed(seed);
         
-        for (int i = 0; i < u.length; ++i) {
-            u[i] /= sqSumSq;
+        return generateUnitStandard(d, rand, false);
+    }
+    
+    /**
+     * generate random uniform points on an n-dimensional unit standard hypersphere
+     * (for onSurface=true) or in an n-dimensional unit standard ball.
+     * NOTE: to generate multiple samples, invoke the overloaded method multiple times.
+     * <pre>
+     * paper containing references:
+     * "Efficiently sampling vectors and coordinates from the n-sphere and n-ball"
+    Aaron R. Voelker, Jan Gosmann, Terrence C. Stewart Centre for Theoretical Neuroscience – Technical Report
+    January 4, 2017
+     * https://www.researchgate.net/profile/Aaron_Voelker/publication/312056739_Efficiently_sampling_vectors_and_coordinates_from_the_n-sphere_and_n-ball/links/586d3e1208ae6eb871bce1d6/Efficiently-sampling-vectors-and-coordinates-from-the-n-sphere-and-n-ball.pdf
+     * </pre>
+     * @param d number of dimensions.
+     * @param rand
+     * @param onSurface
+     * @return vector of size d+1 for onSurface == true, else vector of size d
+     * @throws NoSuchAlgorithmException 
+     */
+    public static double[] generateUnitStandard(int d, SecureRandom rand, boolean onSurface) 
+        throws NoSuchAlgorithmException {
+        
+        if (onSurface) {
+            d++;
         }
         
         /*
-        // the sas blog uses a radius factor too:
-        // r = radius * u##(1/d);
-        //   which means r = radius * U(0,1) of dimension nDimensions to the power (1/d)
-        //      so a vector u_n of (1)^(1/d) is all 1's(??), then multiplying by radius
-        //      which is 1 here
-       
+        also see the SAS blog for similar algorithm of d dimensions and N samples:
         https://blogs.sas.com/content/iml/2016/04/06/generate-points-uniformly-in-ball.html
-        NOTE: https://support.sas.com/rnd/app/iml/matrix.html
-        multiplication operator, elementwise:   #
-        multiplication operator, matrix:   *
-        power operator, elementwise:   ##
-        power operator, matrix:   **
-        
-        proc iml;
-        call randseed(12345);
-        d = 3;                               // dimension = number of variables 
-        N = 1000;                            // sample size = number of obs    
         radius = 2;                          // radius of circle 
         Y = randfun(N // d, "Normal");       // Y ~ MVN(0, I(d)) 
         u = randfun(N, "Uniform");           // U ~ U(0,1)       
@@ -75,11 +108,30 @@ public class MultivariateUniformDistribution {
         // X contains N random uniformly distributed points in the d-bal
         */
         
+        // note: Voelker et al. 2017 demonstrate with a python library called Nengo
+       
+        // generate d dimensions individ random standard normal N(0,1)  
+        double[] u = UnivariateNormalDistribution.randomSampleOfUnitStandard(rand, d);
+        
+        // then normalization of each is the sqrt(sum of squares of all)
+        double norm = 0;
+        for (double v : u) {
+            norm += (v*v);
+        }
+        norm = 1./Math.sqrt(norm);
+        
+        if (!onSurface) {
+            // to avoid bunching at the center of hypersphere
+            double b = Math.pow(rand.nextDouble(), 1./d);
+        
+            norm *= b;
+        }
+        
+        for (int i = 0; i < u.length; ++i) {
+            u[i] *= norm;
+        }
+                
         return u;
     }
     
-    /*
-    from https://rdrr.io/cran/NonNorMvtDist/man/MvtUniform.html
-    
-    */
 }
--- src/test/java/algorithms/sampling/MultivariateUniformDistributionTest.java
+++ src/test/java/algorithms/sampling/MultivariateUniformDistributionTest.java
@@ -15,6 +15,12 @@ public class MultivariateUniformDistributionTest extends TestCase {
     public void test0() {
         // see https://www.researchgate.net/publication/283875292_Multivariate_tests_of_uniformity
         
+        //M^2, Q3, L_MST
+        // (MS2) Multivariate Hegazy–Green tests using spherical (MS2)
+        // (ML2) lens  
+        // (MT2) random Tukey depth functions, 
+        // (LMST) total MST weight , 
+        // and interpoint distances of uniforms (Q1, Q2, and Q3),
         // and Ripley's K function
     }
     
