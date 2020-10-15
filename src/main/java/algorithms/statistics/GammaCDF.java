ffec724b28ad0f6c712900b293c069583fa69b7c
nichole <nichole@climbwithyourfeet.com>
2020-10-15 12:46:36 -0700

    editing gamma cdf argument exceptions and variables

--- src/main/java/algorithms/statistics/GammaCDF.java
+++ src/main/java/algorithms/statistics/GammaCDF.java
@@ -9,21 +9,18 @@ public class GammaCDF {
     public static double cdf(double x, double shape, double scale) {
      
         // from https://www.mathworks.com/help/stats/gamcdf.html
-        
-        double a = shape;
-        double b = scale;
-        
+                
         if (x < 0) {
             throw new IllegalArgumentException("x cannot be a negative number");
         }
-        if (a <= 0) {
-            throw new IllegalArgumentException("a must be a positive number");
+        if (shape <= 0) {
+            throw new IllegalArgumentException("shape must be a positive number");
         }
-        if (b <= 0) {
-            throw new IllegalArgumentException("b must be a positive number");
+        if (scale <= 0) {
+            throw new IllegalArgumentException("scale must be a positive number");
         }
         
-        return thirdparty.smile.math.special.Gamma.regularizedIncompleteGamma(a, x/b);
+        return thirdparty.smile.math.special.Gamma.regularizedIncompleteGamma(shape, x/scale);
     }
     
     public static double inverseCdf(double shape, double scale, double alpha) {
