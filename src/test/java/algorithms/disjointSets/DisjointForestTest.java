e1abdba7b47815ee306a6fb11c3c5a6efaf5b9e1
nichole <nichole@climbwithyourfeet.com>
2020-11-27 13:46:41 -0800

    fixing a typo in a unit test

--- src/test/java/algorithms/disjointSets/DisjointForestTest.java
+++ src/test/java/algorithms/disjointSets/DisjointForestTest.java
@@ -32,25 +32,25 @@ public class DisjointForestTest extends TestCase {
         DisjointForest.RootedTreeDisjointSet<String> treeC = forest.makeSet(c);
         DisjointForest.RootedTreeDisjointSet<String> treeE = forest.makeSet(e);
         DisjointForest.RootedTreeDisjointSet<String> treeH = forest.makeSet(h);
-        assertEqauls(4, forest.getTrees().size());
+        assertEquals(4, forest.getTrees().size());
         
         System.out.println("\nforest(b,c,e,h) before any unions:\n" + forest.toString());
         
         DisjointSet2Node<String> bh = forest.union(b, h);
         System.out.println("\nforest(b,c,e,h) after any union b,h:\n" + forest.toString());
-        assertEqauls(3, forest.getTrees().size());
+        assertEquals(3, forest.getTrees().size());
         
         DisjointSet2Node<String> ec = forest.union(e, c);
         System.out.println("\nforest(b,c,e,h) after any union e,c:\n" + forest.toString());
-        assertEqauls(2, forest.getTrees().size());
+        assertEquals(2, forest.getTrees().size());
         
         DisjointSet2Node<String> bhec = forest.union(b, e);
-        assertEqauls(1, forest.getTrees().size());
+        assertEquals(1, forest.getTrees().size());
         System.out.println("\nforest(b,c,e,h) after any union all:\n" + forest.toString());
         
         // findSet compresses c's path to point to c's earliest ancestor parent
         DisjointSet2Node<String> parentPOfC = forest.findSet(c);
         System.out.println("\nforest(b,c,e,h) after findSet(c):\n" + forest.toString());
-        assertEqauls(1, forest.getTrees().size());
+        assertEquals(1, forest.getTrees().size());
     }
 }
