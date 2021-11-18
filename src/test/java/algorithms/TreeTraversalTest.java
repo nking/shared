60fc191df47e7147ec8d2df2d0bf62ff9bf6c45a
nichole <nichole@climbwithyourfeet.com>
2021-11-17 18:46:28 -0800

    corrected the in-order tree traversal

--- src/test/java/algorithms/TreeTraversalTest.java
+++ src/test/java/algorithms/TreeTraversalTest.java
@@ -116,15 +116,15 @@ public class TreeTraversalTest extends TestCase {
     /**
      * left subtree, root, right subtree
      <pre>
-       e.g.
-               0
-            1         2
-           3  4      5  6
-         7  
-         
-     recursive visits: 1, 3, 7, 4, 0, 2, 5, 6
-     iterative visits: 7, 3, 1, 4, 0, 5, 2, 6
-     </pre>
+     e.g.
+                0
+            1           2
+           3  4      5     6
+         7     10     8      11
+                        9   12 13
+                        
+     recursive visits: 1, 3, 7, 4, 10, 0, 2, 5, 8, 9, 6, 11, 12, 13
+     iterative visits: 1, 3, 7, 4, 10, 0, 2, 5, 8, 9, 6, 11, 12, 13
      */
     public void inorderIterative(Node node) {
         /*Stack<Node> stack = new Stack<>();
@@ -140,41 +140,53 @@ public class TreeTraversalTest extends TestCase {
         }
         System.out.println();
         */
-//TODO: visiting right subtree is not correct yet     
         Stack<Node> stack = new Stack<>();
         Stack<Node> stackL = new Stack<>();
-        Node p;
+        Stack<Node> stackR = new Stack<>();
+        System.out.println();
         while (!stack.isEmpty() || node != null) {
             if (node != null) {
                 stack.add(node);
                 node = node.left;
             } else {
                 node = stack.pop(); 
-                while (node.right == null && 
-                    node.parent != null && !stack.isEmpty() && (stack.peek().data == node.parent.data)) {
-                    stackL.add(node);
-                    node = stack.pop();
-                    if (node.right != null) {
-                        break;
+                if (node.right == null) {
+                    while (node.right == null && 
+                        node.parent != null && !stack.isEmpty() && (stack.peek().data == node.parent.data)) {
+                        stackL.add(node);
+                        node = stack.pop();
+                        if (node.right != null) {
+                            break;
+                        }
+                    }
+                } else if (node.left == null) {
+                    while (node.left == null && 
+                        node.parent != null && !stack.isEmpty() && (stack.peek().data == node.parent.data)) {
+                        stackR.add(node);
+                        node = stack.pop();
+                        if (node.left != null) {
+                            break;
+                        }
                     }
                 }
                 System.out.printf("%d, ", node.data);
-                node = node.right;
                 while (!stackL.isEmpty()) {
                     Node n2 = stackL.pop();
                     System.out.printf("%d, ", n2.data);
                 }
+                if (!stackR.isEmpty()) {
+                    if (node.right != null) {
+                        stack.add(node.right);
+                    }
+                    while (!stackR.isEmpty()) {
+                        node = stackR.pop();
+                        System.out.printf("%d, ", node.data);
+                    }
+                }
+                node = node.right;
             }
         }
         System.out.println();
-        /*
-                0
-            1           2
-           3  4      5     6
-         7            8
-                        9
-        
-        */
     }
 
     /**
@@ -314,7 +326,7 @@ public class TreeTraversalTest extends TestCase {
         reverseLevelOrderIterative(nodes[0]);
     }
     
-    public void test1() {
+    public void est1() {
 
         /*
                 0
@@ -347,6 +359,67 @@ public class TreeTraversalTest extends TestCase {
         nodes[6].parent = nodes[2];
         nodes[8].parent = nodes[5];
         nodes[9].parent = nodes[8];
+
+        System.out.println("pre-order:");
+        preorderRecursive(nodes[0]);
+        preorderIterative(nodes[0]);
+        
+        System.out.println("in-order:");
+        inorderRecursive(nodes[0]);
+        inorderIterative(nodes[0]);
+
+        System.out.println("post-order:");
+        postorderRecursive(nodes[0]);
+        postorderIterative(nodes[0]);
+        
+        System.out.println("level-order:");
+        levelOrderIterative(nodes[0]);
+
+        System.out.println("reverse-level-order:");
+        reverseLevelOrderIterative(nodes[0]);
+    }
+    
+    public void test2() {
+
+        /*
+                0
+            1           2
+           3  4      5     6
+         7     10     8      11
+                        9   12 13
+         */
+        Node[] nodes = new Node[14];
+        int i;
+        for (i = 0; i < nodes.length; ++i) {
+            nodes[i] = new Node(i);
+        }
+        nodes[0].left = nodes[1];
+        nodes[0].right = nodes[2];
+        nodes[1].left = nodes[3];
+        nodes[1].right = nodes[4];
+        nodes[2].left = nodes[5];
+        nodes[2].right = nodes[6];
+        nodes[3].left = nodes[7];
+        nodes[4].right = nodes[10];
+        nodes[5].right = nodes[8];
+        nodes[6].right = nodes[11];
+        nodes[8].right = nodes[9];
+        nodes[11].left = nodes[12];
+        nodes[11].right = nodes[13];
+        
+        nodes[1].parent = nodes[0];
+        nodes[2].parent = nodes[0];
+        nodes[3].parent = nodes[1];
+        nodes[4].parent = nodes[1];
+        nodes[7].parent = nodes[3];
+        nodes[5].parent = nodes[2];
+        nodes[6].parent = nodes[2];
+        nodes[8].parent = nodes[5];
+        nodes[9].parent = nodes[8];
+        nodes[10].parent = nodes[4];
+        nodes[11].parent = nodes[6];
+        nodes[12].parent = nodes[11];
+        nodes[13].parent = nodes[11];
 
         System.out.println("pre-order:");
         preorderRecursive(nodes[0]);
