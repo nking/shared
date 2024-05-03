0af8a4d485c76baf766c33fb20458ed1c0af278b
nking <nichole climbwithyourfeet.com>
2024-05-03 10:09:07 -0700

    shift-reduce parser
    created a simpler version of a shift-reduce parser to evaluate algebraic
    expressions.

--- src/main/java/algorithms/misc/AlgebraicExpressionEvaluator2.java
+++ src/main/java/algorithms/misc/AlgebraicExpressionEvaluator2.java
@@ -3,13 +3,14 @@ package algorithms.misc;
 import java.util.*;
 
 /**
- *
- * class to evaluate an algebraic expression using a reduce-shift parser pattern to build parse tree
- * and then uses reverse level order traversal to evaluate the parse tree.
+ class to evaluate an algebraic expression using a shift-reduce parse pattern to build
+ a parse tree. The tree nodes are evaluated in the reduce stages.
 
  @author Nichole
  */
-public class AlgebraicExpressionEvaluator {
+public class AlgebraicExpressionEvaluator2 {
+
+    protected static double sentinel = Double.POSITIVE_INFINITY;
 
     protected static Map<Character, Integer> opAlgPriorityMap = new HashMap<>();
     static {
@@ -72,59 +73,34 @@ public class AlgebraicExpressionEvaluator {
         int i = 0;
         while (i < j && Character.isSpaceChar(chars[i])) ++i;
 
-        OpPair root = buildExprTree(chars, i, j);
-
-        return reverseLevelOrderEval(root, chars);
+        return parseAndEvaluate(chars, i, j);
     }
 
     /**
-     * build an algebraic expression tree from the input string that has infix format.
-     *
+     * build an algebraic expression tree from the input string that has infix format and evaluate
+     * the expression in the reduce stages.
+
      <pre>
      This method is adapted from code available at
      https://www.geeksforgeeks.org/program-to-convert-infix-notation-to-expression-tree/
      authored by Julkar9, and aashish1995
+     but edited to add evaluation.
+
      </pre>
      * @param chars
      * @param i
      * @param j
      * @return
      */
-    protected OpPair buildExprTree(char[] chars, int i, int j) {
+    protected double parseAndEvaluate(char[] chars, int i, int j) {
 
         //numberStack is the equiv of the postfix format string
-        Stack<OpPair> numberStack = new Stack<>();
+        Stack<Double> numberStack = new Stack<>();
         Stack<CRange> charStack = new Stack<>();
 
-        OpPair t;
-        int[] i01 = new int[2];
-        /*
-        while (i <= j) {
-            if c == '('
-                cStack.push(d)
-            else if c == ')' {
-                while (!cStack.isEmpty() && cStack.peek != '(') {
-                    t = new OpPair(cStack.pop())
-                    t.right = nStack.pop();
-                    t.left = nStack.pop();
-     =>              nStack.push(t);
-                }
-                cStack.pop();//this is the opening parenthesis
-            } else if c is an operator {
-                while (!cStack.isEmpty() && cStack.peek != '(' && obeys precedence) {
-                    t = new OpPair(cStack.pop());
-                    t.right = nStack.pop();
-                    t.left = nStack.pop();
-      -->            nStack.push(t);
-                }
-                cStack.push(new CRange(i, i));
-            } else {
-                c is a number
-      =>          nStack.push(new OPair(i,i))
-            }
+        double result = 0.;
 
-        }
-         */
+        int[] i01 = new int[2];
 
         boolean prevWasNumber = false;
         while (i <= j) {
@@ -139,11 +115,8 @@ public class AlgebraicExpressionEvaluator {
                 // a Reduce stage of shift-reduce parsing
 
                 while (!charStack.isEmpty() && isNotLeftParenthesis(charStack.peek(), chars)) {
-
-                    t = new OpPair(charStack.pop());
-                    t.right = numberStack.pop();
-                    t.left = numberStack.pop();
-                    numberStack.push(t);
+                    result = evaluate(chars, charStack.pop(), numberStack.pop(), numberStack.pop());
+                    numberStack.push(result);
                 }
                 // pop the opening parenthesis:
                 charStack.pop();
@@ -157,13 +130,11 @@ public class AlgebraicExpressionEvaluator {
                     && ((chars[i] != '^' && priorityisGEQ(charStack.peek(), i, chars))
                     || (chars[i] == '^' && priorityisGT(charStack.peek(), i, chars)))) {
 
-                    t = new OpPair(charStack.pop());
-                    t.right = numberStack.pop();
-                    t.left = numberStack.pop();
-
-                    numberStack.push(t);
+                    result = evaluate(chars, charStack.pop(), numberStack.pop(), numberStack.pop());
+                    numberStack.push(result);
                 }
                 charStack.push(new CRange(i, i));
+
             } else {
 
                 // a Shift stage of shift-reduce parsing
@@ -174,8 +145,7 @@ public class AlgebraicExpressionEvaluator {
                 parseForNumber(i, j, chars, i01);
                 i = i01[1];
 
-                t = new OpPair(new CRange(i01[0], i01[1]));
-                numberStack.push(t);
+                numberStack.push( parseNumber(chars, new CRange(i01[0], i01[1])) );
             }
 
             ++i;
@@ -186,7 +156,38 @@ public class AlgebraicExpressionEvaluator {
 
         assert(charStack.isEmpty());
         assert(numberStack.size() == 1);
-        return numberStack.pop();
+        return result;
+    }
+
+    private double evaluate(char[] chars, CRange op, double vRight, double vLeft) {
+        double result = 0;
+
+        switch(chars[op.i0]) {
+            case '^': {
+                result = Math.pow(vLeft, vRight);
+                break;
+            }
+            case '*': {
+                result = vLeft * vRight;
+                break;
+            }
+            case '/': {
+                result = vLeft / vRight;
+                break;
+            }
+            case '+': {
+                result = vLeft + vRight;
+                break;
+            }
+            case '-': {
+                result = vLeft - vRight;
+                break;
+            }
+            default:
+                throw new IllegalArgumentException("ERROR: operator not recognized: " + chars[op.i0]);
+        }
+
+        return result;
     }
 
     protected void parseForNumber(int i, int j, char[] chars, int[] outputIdxs) {
@@ -203,86 +204,8 @@ public class AlgebraicExpressionEvaluator {
         outputIdxs[1] = i;
     }
 
-    protected double reverseLevelOrderEval(OpPair node, char[] chars) {
-        Queue<OpPair> q = new ArrayDeque<>();
-        Stack<OpPair> s = new Stack<>();
-        q.offer(node);
-
-        while (!q.isEmpty()) {
-            node = q.poll();
-            s.push(node);
-            if (node.left != null) q.offer(node.left);
-            if (node.right != null) q.offer(node.right);
-        }
-
-        // key = node hashcode, value=result
-        Map<Integer, Double> resultsMap = new HashMap<>();
-
-        double result=0;
-        char op;
-        while (!s.isEmpty()) {
-            node = s.pop();
-
-            if (node.left == null && node.right==null) {
-                // leaf nodes have literals stored in data
-                resultsMap.put(node.hashCode(), parseNumber(chars, node.data));
-            } else {
-                assert(node.data.i0 == node.data.i1);
-                op = chars[node.data.i0];
-                double v1;
-
-                if (resultsMap.containsKey(node.left.hashCode())) {
-                    v1 = resultsMap.get(node.left.hashCode());
-                } else {
-                    v1 = parseNumber(chars, node.left.data);
-                }
-                double v2;
-                if (resultsMap.containsKey(node.right.hashCode())) {
-                    v2 = resultsMap.get(node.right.hashCode());
-                } else {
-                    v2 = parseNumber(chars, node.right.data);
-                }
-
-                switch(op) {
-                    case '^': {
-                        result = Math.pow(v1, v2);
-                        resultsMap.put(node.hashCode(), result);
-                        break;
-                    }
-                    case '*': {
-                        result = v1 * v2;
-                        resultsMap.put(node.hashCode(), result);
-                        break;
-                    }
-                    case '/': {
-                        result = v1 / v2;
-                        resultsMap.put(node.hashCode(), result);
-                        break;
-                    }
-                    case '+': {
-                        result = v1 + v2;
-                        resultsMap.put(node.hashCode(), result);
-                        break;
-                    }
-                    case '-': {
-                        result = v1 - v2;
-                        resultsMap.put(node.hashCode(), result);
-                        break;
-                    }
-                    default:
-                        throw new IllegalArgumentException("ERROR: operator not recognized: " + node.data);
-                }// end switch
-            }// end else
-
-        }//end while
-        return result;
-    }
     public String toString(char[] chars, CRange cRange) {
         return String.copyValueOf(chars, cRange.i0, cRange.i1 - cRange.i0 + 1);
-    }
-
-    protected double parseNumber(char[] chars, OpPair node) {
-        return parseNumber(chars, node.data);
     }
 
     protected double parseNumber(char[] chars, CRange nodeData) {
@@ -305,15 +228,6 @@ public class AlgebraicExpressionEvaluator {
 
     protected boolean isNotLeftParenthesis(CRange c, char[] chars) {
         return !(c.i0 == c.i1 && chars[c.i0] == '(');
-    }
-
-    protected static class OpPair {
-        OpPair left = null;
-        OpPair right = null;
-        CRange data;
-        public OpPair(CRange cr) {
-            this.data = cr;
-        }
     }
 
     protected static class CRange {
