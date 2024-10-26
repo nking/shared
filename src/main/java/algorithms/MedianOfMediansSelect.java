987a1f88c670e3bd4c1cc5e56f8d8fc3388c1278
nking <nichole@climbwithyourfeet.com>
2024-10-26 08:29:17 -0700

    restoring MedianOfMediansSelect to follow the CLRS pseudocode now that have the 0-based indexing changes correct.  keeping the less efficient select method for a look at why it needed to look at nRem size parity.

--- src/main/java/algorithms/MedianOfMediansSelect.java
+++ src/main/java/algorithms/MedianOfMediansSelect.java
@@ -38,7 +38,7 @@ public class MedianOfMediansSelect {
      * @param a
      * @param idxLo smallest index to of range
      * @param idxHi largest index of range, inclusive
-     * @param i the rank of the item to select in 0-based numbering.
+     * @param i the rank of the item to selectLessEfficient in 0-based numbering.
      * @return the value of a's rank i item where i is 0-based
      */
     static double selectCLRS(double[] a, int idxLo, int idxHi, int i) {
@@ -78,7 +78,7 @@ public class MedianOfMediansSelect {
      * @param a
      * @param idxLo smallest index to of range
      * @param idxHi largest index of range, inclusive
-     * @param i the rank of the item to select in 0-based numbering.
+     * @param i the rank of the item to selectLessEfficient in 0-based numbering.
      * @return the median
      */
     private static double select(double[] a, int idxLo, int idxHi, int i) {
@@ -87,7 +87,7 @@ public class MedianOfMediansSelect {
         int _idxHi = idxHi;
         final int _i = i;
 
-        //System.out.printf("* select idxLo=%d; idxHi=%d; i=%d\n", idxLo, idxHi, i);
+        //System.out.printf("* selectLessEfficient idxLo=%d; idxHi=%d; i=%d\n", idxLo, idxHi, i);
 
         while ((idxHi - idxLo + 1) %5 != 0) {
             for (int j = idxLo + 1; j <= idxHi; ++j) {
@@ -98,7 +98,6 @@ public class MedianOfMediansSelect {
                 }
             }
             if (i == 0 && (idxHi - idxLo) < 5) {
-                //System.out.printf("return 0\n");
                 return a[idxLo];
             }
             ++idxLo;
@@ -133,13 +132,98 @@ public class MedianOfMediansSelect {
             return a[idxLo + i];
         }
 
-        //System.out.printf("NEXT select 1\n");
-        //System.out.printf("a=%s\n", FormatArray.toString(a, "%.0f"));
-
         // all group medians now lie in the middle fifth of A[idxLo:idcHi]
 
         int nextI = g - 1;//(int)Math.ceil(g/2);//((idxHi - idxLo) + 4) / 5;
         double x = select(a, idxLo + 2*g, idxLo + 3*g - 1, nextI);
+
+        int q = partitionAround(a, idxLo, idxHi, x);
+        // q is pivotIndex w.r.t 0
+        // k is its rank
+        int k = q - idxLo;
+
+        double result;
+        if (k==i) {
+            result = a[q];
+        } else if (k>i) {
+            result = select(a, idxLo, q - 1, i);
+        } else {
+            result = select(a, q + 1, idxHi, i - k - 1);
+        }
+
+        return result;
+    }
+
+    private static double selectLessEfficient(double[] a, int idxLo, int idxHi, int i) {
+
+        int n = (idxHi - idxLo + 1);
+
+        final int _idxLo = idxLo;
+        int _idxHi = idxHi;
+        final int _i = i;
+
+        int g = n/5;
+        int nRem = n - g*5;
+
+        //System.out.printf("* selectLessEfficient idxLo=%d; idxHi=%d; i=%d; g=%d; nRem=%d\n", idxLo, idxHi, i, g, nRem);
+
+        if (idxHi < idxLo) {
+            throw new IllegalArgumentException("   ERROR idxHi<idxLo\n");
+        }
+
+        for (int j = idxLo; j <= (idxLo + g - 1); ++j) {
+            quickSort5(a, j, j + g*4, g);
+            // sort each group (A[j], A[j+g], A[j+2g], A[j+3g, A[j+4g] in place
+            // length is 5.
+            // sort in place algoriths: quicksort, insertsort, bubblesort, selectionsort
+            // all but the first are n^2.  quicksort is O(n*log(n)) on average.
+        }
+        //debugPrint(a, idxLo, idxHi, g);
+
+        //Blum et al. 1973 1.b, pick recursively if n/5 > 1
+        if (nRem==0 && g < 2 && (idxHi - idxLo) < 5) {
+            //System.out.printf("   **** i=%d; idxLo=%d; idxHi=%d; g=%d, nRem=%d\n    a=%s\n",
+            //        i, idxLo, idxHi, g, nRem,
+            //        FormatArray.toString(a, "%.0f"));
+            return a[idxLo + i];
+        }
+
+        //System.out.printf("NEXT selectLessEfficient 1\n");
+        //System.out.printf("a=%s\n", FormatArray.toString(a, "%.0f"));
+
+        // all group medians now lie in the middle fifth of A[idxLo:idcHi]
+
+        // handle the remaining numbers that were not part of the g groups of size 5:
+        // calc the median of the remaining numbers as xRem.
+        // TODO: this could be improved by passing the method an auxilliary array and only using as much of it as needed
+
+        // create the auxilliary array to hold the medians of the groups just sorted, and the median of the remaining numbers
+        int nAux = g;
+        double[] aux;
+        if (nRem == 0) {
+            aux = new double[nAux];
+        } else {
+            Arrays.sort(a, idxHi - nRem + 1, idxHi + 1);
+            ++nAux;
+            aux = new double[nAux];
+            aux[nAux - 1] = a[ (idxHi-nRem+1) + (nRem/2)];
+        }
+        int _ii = 0;
+        for (int ii = idxLo + 2*g; ii <= idxLo + 3*g - 1; ++ii) {
+            aux[_ii++] = a[ii];
+        }
+
+        //System.out.printf("check aux=%s\n", FormatArray.toString(aux, "%.0f"));
+
+        //int nextI = (int)Math.ceil(g/2);//((idxHi - idxLo) + 4) / 5;
+        //double x = selectLessEfficient(a, idxLo + 2*g, idxLo + 3*g - 1, nextI);
+        double x;
+        if (nAux == 1) {
+            x = aux[0];
+        } else {
+            //Blum et al. 1973 1.b, pick recursively if n/5 > 1
+            x = selectLessEfficient(aux, 0, nAux - 1, nAux/2);
+        }
 
         //if nAux == even number, we should consider both central numbers.  the other is at index (nAux/2) - 1.
         // or consider whether there is a way to append another number to aux (making the array 'odd' in length)
@@ -159,15 +243,42 @@ public class MedianOfMediansSelect {
 
         double result;
         if (k==i) {
-            //System.out.printf("NEXT select 2 (==q)\n");
+            //System.out.printf("NEXT selectLessEfficient 2 (==q)\n");
             result = a[q];
         } else if (k>i) {
-            //System.out.printf("NEXT select 3 (lower)\n");
-            result = select(a, idxLo, q - 1, i);
+            //System.out.printf("NEXT selectLessEfficient 3 (lower)\n");
+            result = selectLessEfficient(a, idxLo, q - 1, i);
         } else {
-            //System.out.printf("NEXT select 4 (higher)\n");
-            //return select(a, q + 1, idxHi, i-k);
-            result = select(a, q + 1, idxHi, i - k - 1);
+            //System.out.printf("NEXT selectLessEfficient 4 (higher)\n");
+            //return selectLessEfficient(a, q + 1, idxHi, i-k);
+            result = selectLessEfficient(a, q + 1, idxHi, i - k - 1);
+        }
+
+        if ((nAux & 1) == 0 && nAux > 1) {
+            // for even sized aux arrays, try the other central value as the median
+            //System.out.printf("trying the other median of the even-sized aux array\n");
+            double x2 = aux[(nAux/2) - 1];
+            int q2 = partitionAround(a, idxLo, idxHi, x2);
+
+            int k2 = q2 - idxLo;
+
+            //System.out.printf("pivotIdx2 = q2 = %d; pivot2=%.0f; k2=\n", q2, x2, k2);
+            //System.out.printf("a=%s\n", FormatArray.toString(a, "%.0f"));
+
+            double result2;
+            if (k2==i) {
+                //System.out.printf("*NEXT selectLessEfficient 2 (==q2)\n");
+                result2 = a[q2];
+            } else if (k2>i) {
+                //System.out.printf("*NEXT selectLessEfficient 3 (lower)\n");
+                result2 = selectLessEfficient(a, idxLo, q2 - 1, i);
+            } else {
+                //System.out.printf("*NEXT selectLessEfficient 4 (higher)\n");
+                //return selectLessEfficient(a, q + 1, idxHi, i-k);
+                result2 = selectLessEfficient(a, q2 + 1, idxHi, i - k2 - 1);
+            }
+            //System.out.printf("COMPARE result=%.0f; result2=%.0f\n", result, result2);
+            result = result2;
         }
 
         return result;
