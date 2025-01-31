81cb0725760bcb0c9a8de845035b9bb6fa433bfe
nking <nichole@climbwithyourfeet.com>
2025-01-31 09:34:03 -0800

    removing unused imports in a class

--- src/main/java/algorithms/trees/SegmentTreeFreq.java
+++ src/main/java/algorithms/trees/SegmentTreeFreq.java
@@ -1,9 +1,6 @@
 package algorithms.trees;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SegmentTreeFreq {
 
